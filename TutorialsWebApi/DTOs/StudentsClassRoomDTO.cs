dba722b2c460df7f653f0bb4441c5ce4be12381a
Daniel Barrios <danielbarrios76@gmail.com>
2018-10-13 00:47:03 -0300

    [#2] Issue02 add Data Transfer Object

---
+++ TutorialsWebApi/DTOs/StudentsClassRoomDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TutorialsWebApi.DTOs
+{
+    public class StudentsClassRoomDTO
+    {
+        public int Id { get; set; }
+        public int ClassRoomId { get; set; }
+        public int StudentsId { get; set; }
+
+        public ClassRoomDTO ClassRoom { get; set; }
+        public StudentsDTO Students { get; set; }
+    }
+}
---
+++ TutorialsWebApi/DTOs/SubjectsDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TutorialsWebApi.DTOs
+{
+    public class SubjectsDTO
+    {
+        public int Id { get; set; }
+        public string SubjectsName { get; set; }
+
+        public List<ClassRoomDTO> ClassRoom { get; set; }
+    }
+}
